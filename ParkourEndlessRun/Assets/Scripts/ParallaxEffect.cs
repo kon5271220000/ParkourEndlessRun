0b7f9fab221e423a17e490288e83ba56362ced86
kon5271220000 <kon527122000@gmail.com>
2024-04-10 13:25:48 +0700

    camera+parallax background

---
+++ ParkourEndlessRun/Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    private GameObject cam;
+
+    [SerializeField] private float parallaxEffect;
+
+    private float length;
+    private float xPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+
+        xPosition = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToMove = cam.transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(xPosition + distanceToMove, transform.position.y);
+    }
+}
